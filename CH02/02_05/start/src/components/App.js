d1ea7563380de514744b8e145a45198160bee783
Louise Hjelstroem <Louiseh_83@hotmail.com>
2019-04-11 17:04:39 +0200

    Ändrade lite på app-titeln

--- CH02/02_05/start/src/components/App.js
+++ CH02/02_05/start/src/components/App.js
@@ -21,7 +21,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">My feed</h1>
+          <h1 className="App-title">My latest news</h1>
         </header>
         <News news={this.state.news1} />
         <News news={this.state.news2} />
